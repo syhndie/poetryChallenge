26c6a3239f80a73dc38972c63926e3d4c903cc76
Cindy <syhndie2@gmail.com>
2021-10-02 15:56:18 -0500

    poet tags created with window.onload function

--- poetlist.js
+++ poetlist.js
@@ -1,32 +1,21 @@
-const poetsDiv = document.getElementById('poets');
-const poetRes = axios.get('https://poetrydb.org/author')
-.then((poetRes) => {
-    const poetData = poetRes.data;
-    if (poetData.status) {
-        console.log(`problem with response: ${poetData.status}`);
-    } else {
-        const poets = poetData.authors;
-        for (let poet of poets) {
-            // const checkbox = document.createElement('input');
-            // checkbox.value = poet;
-            // checkbox.type = 'checkbox';
-            // checkbox.name = 'poets';
-            // checkbox.id = poet;
-
-            // const label = document.createElement('label');
-            // label.htmlFor = poet;
-            // label.innerText = poet;
-
-            const tag = document.createElement('span');
-            tag.classList.add('tag');
-            tag.classList.add('is-rounded');
-            tag.innerText = poet;
-            // tag.appendChild(checkbox);
-            // tag.appendChild(label); 
-            poetsDiv.appendChild(tag);
-
-
+const populatePoets = () => {
+    axios.get('https://poetrydb.org/author')
+    .then((res) => {
+        if (res.data.status) {
+            console.log('boo');
+        } else {
+            const poetsTags = document.getElementById('poets-tags');
+            const poets = res.data.authors;
+            for (let poet of poets) {
+                const tag = document.createElement('span');
+                tag.classList.add('tag');
+                tag.classList.add('is-rounded');
+                tag.innerText = poet;
+                poetsTags.appendChild(tag);
+            }
         }
-    }
-})
-.catch((err) => {console.log(`oops: ${err}`);});
+    })
+    .catch((err) => {
+        console.log('ouch');
+    });
+};
