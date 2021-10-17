ec1a865bf323b0608b02d550e23612bc35a1a709
Cindy <syhndie2@gmail.com>
2021-10-17 11:10:07 -0500

    submit button gets a random poem from each selected poet

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-import {populatePoets} from "./modules/poetlist.js";
+import {populatePoets, submitPoets} from "./modules/poetlist.js";
 
 const poetsToCompare = 4;
 
 window.addEventListener('load', () => {
     populatePoets(poetsToCompare);
+});
+
+const poetsSubmitBtn = document.getElementById('poets-submit')
+poetsSubmitBtn.addEventListener('click', (e) => {
+    submitPoets();
 });
--- modules/poetlist.js
+++ modules/poetlist.js
@@ -29,3 +29,21 @@ export function populatePoets(poetsNumber) {
         displayError('poets-error', 'There was a problem connecting to the Poetry DB', err);
     });
 };
+
+export function submitPoets() {
+    const poetButtons = document.querySelectorAll('button.is-selected');
+    for (let poetButton of poetButtons) {
+        const axiosString = `https://poetrydb.org/author,random/${poetButton.innerText};1`;
+        axios.get(axiosString)
+        .then((res) => {
+            if(res.data.status){
+                displayError('poets-error', 'The PoetryDB request did not return a result', res.data.status);
+            } else {
+                console.log(res.data);
+            }
+        })
+        .catch((err) => {
+            displayError('poets-error', 'There was a problem connecting to the Poetry DB', err);
+        });
+    }
+};
