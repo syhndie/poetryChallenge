fa1518f112da3ef5b9029ab3ddba7312ed1622f9
Cindy <syhndie2@gmail.com>
2021-11-02 15:11:53 -0500

    added shuffle function to almost randomly sort poem array

--- modules/utils.js
+++ modules/utils.js
@@ -46,3 +46,17 @@ export function removeButtons() {
     const submitButton = document.getElementById('poets-submit-button');
     submitButtonDiv.removeChild(submitButton);
 };
+
+export function shuffle(array) {
+    let counter = array.length;
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        counter--;
+
+        const temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+};
+
