89330984b467b3657b6153f87be4f9b52cdf1bfe
Cindy <syhndie2@gmail.com>
2021-11-06 15:17:32 -0500

    poet and title buttons created on submitting poet choices

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import{ initialSetup } from "./modules/initialSetup.js";
 
 const numberOfMatches = 4;
+const colorClasses = ['is-green', 'is-pink', 'is-purple', 'is-yellow'];
 
 window.addEventListener('load', () => {
     initialSetup(numberOfMatches);
--- modules/gameSetup.js
+++ modules/gameSetup.js
@@ -1,16 +1,31 @@
 import { shuffle } from "./utils.js";
 
 export function gameSetup(poemInfo) {
+    const colorClasses = ['is-green', 'is-pink', 'is-purple', 'is-yellow'];
     const poetsDiv = document.getElementById('poets-div');
     const titlesDiv = document.getElementById('titles-div');
 
     const poetsShuffled = shuffle(poemInfo);
-    for (let poet of poetsShuffled) {
-        console.log(poet.author, poet.title);   
+    for (let i = 0; i < poetsShuffled.length; i++) {
+        const poet = poetsShuffled[i].author;
+        const btn = document.createElement('button');
+        btn.innerText = poet;
+        btn.classList.add('button', 'is-rounded', 'poet', colorClasses[i]);
+        btn.addEventListener('click', (e) => {
+            console.log(poet);
+        });
+        poetsDiv.appendChild(btn);
     }
 
     const titlesShuffled = shuffle(poemInfo);
-    for (let title of titlesShuffled) {
-        console.log(title.author, title.title);
+    for (let i = 0; i < titlesShuffled.length; i++) {
+        const title = titlesShuffled[i].title;
+        const btn = document.createElement('button');
+        btn.classList.add('button', 'is-rounded', 'poem-title');
+        btn.innerText = title;
+        btn.addEventListener('click', (e) => {
+            console.log(title);
+        });
+        titlesDiv.appendChild(btn);
     }
 };
